cf6a2a6587292583163f22770f4a08774564d9ac
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2025-04-17 12:31:08 +0530

    test

--- api/index.js
+++ api/index.js
@@ -10,3 +10,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/auth', authRoutes);
+app.get('/', (req, res) => {
+    res.send('✅ API working on Vercel');
+  });
